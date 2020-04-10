c2ef8004a6fc93217ac8b41dd83ff97051cd424d
Jesse Langley <jlangleyiii@aol.com>
2020-04-10 07:52:17 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -138,6 +138,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-programmer_hash[:dennis_ritchie][:languages][2] = Assembly"
+programmer_hash[:dennis_ritchie][:languages][2] = "Assembly"
 programmer_hash
 end
