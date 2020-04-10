9b2c16135e78ad44649bf38e73ee8d512405cadb
Jesse Langley <jlangleyiii@aol.com>
2020-04-10 07:51:40 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -138,6 +138,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-programmer_hash[:alan_kay][:known_for] = "GUI"
+programmer_hash[:dennis_ritchie][:known_for] = "GUI"
 programmer_hash
 end
