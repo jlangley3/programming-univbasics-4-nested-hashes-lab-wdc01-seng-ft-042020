8757ff77915f5fd1c15089d00dda97d437b0efd5
Jesse Langley <jlangleyiii@aol.com>
2020-04-10 07:40:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -63,7 +63,7 @@ end
 def adding_matz
 
 
-# add the following information to the top level of programmer_hash
+# add the following information to the top level of :dennis_ritchie
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
 #   :languages => ["LISP", "C"]
